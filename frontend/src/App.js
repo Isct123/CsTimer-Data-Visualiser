ae75e0a9a8c729b3b55dc0f860ba87fd38a8e62f
Isct123 <90542162+Isct123@users.noreply.github.com>
2025-05-30 16:33:56 +0530

    frontend design

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import UploadSolves from "./Components/UploadSolves";
+
+function App() {
+  return (
+    <div className="App">
+      <UploadSolves />
+    </div>
+  );
+}
+
+export default App;
