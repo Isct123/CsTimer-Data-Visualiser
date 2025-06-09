d1e7323bd51602ad6c62355ea600b140ea26b555
Isct123 <90542162+Isct123@users.noreply.github.com>
2025-06-09 17:12:32 +0530

    allowed the web server url

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://cstimer-data-visualiser.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
